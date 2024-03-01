92c3719ab5121d117d8147ad71079909f943bdd4
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-03-01 10:41:54 -0500

    fix git

--- config/fish/aliases.nix
+++ config/fish/aliases.nix
@@ -5,23 +5,6 @@
   dwb = ''darwin-rebuild switch --flake ".#aarch64"'';
   grep = "grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}";
   ks = "tmux kill-server";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  node-shell = "nix shell nixpkgs#{nodejs_21, typescript, eslint_d, prettierd, bun, biome}";
-=======
   node-shell = "nix shell nixpkgs#{nodejs_21, typescript, eslint_d, prettierd, nodePackages.pnpm, bun}";
->>>>>>> Stashed changes
-=======
-  node-shell = "nix shell nixpkgs#{nodejs_21, typescript, eslint_d, prettierd, nodePackages.pnpm, bun}";
->>>>>>> Stashed changes
-=======
-  node-shell = "nix shell nixpkgs#{nodejs_21, typescript, eslint_d, prettierd, nodePackages.pnpm, bun}";
->>>>>>> Stashed changes
-=======
-  node-shell = "nix shell nixpkgs#{nodejs_21, typescript, eslint_d, prettierd, nodePackages.pnpm, bun}";
->>>>>>> Stashed changes
-  kill-opam = "mv ~/.opam ~/_opam";
-  revive-opam = "mv ~/_opam ~/.opam";
+  opam-shell = "nix shell nixpkgs#opam --system aarch64-darwin";
 }
--- config/nvim/lua/plugins/ts_error_translator.lua
+++ config/nvim/lua/plugins/ts_error_translator.lua
@@ -1,10 +1,7 @@
 return {
 	{
 		dir = "~/Code/personal/ts-error-translator.nvim",
-<<<<<<< Updated upstream
 		enabled = false,
-=======
->>>>>>> Stashed changes
 		-- branch = "pr18",
 		config = function()
 			require("ts-error-translator").setup()
