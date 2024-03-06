76ebfbfb2cc908aa9b5bcd41ced9bb1716f33c0a
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-03-05 19:55:58 -0500

    idk what these changes are but yolo

--- config/fish/aliases.nix
+++ config/fish/aliases.nix
@@ -5,6 +5,6 @@
   dwb = ''darwin-rebuild switch --flake ".#aarch64"'';
   grep = "grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}";
   ks = "tmux kill-server";
-  node-shell = "nix shell nixpkgs#{nodejs_21, typescript, eslint_d, prettierd, nodePackages.pnpm, bun}";
+  node-shell = "nix shell nixpkgs#{nodejs_21, typescript, nodePackages.pnpm, bun}";
   opam-shell = "nix shell nixpkgs#opam nixpkgs#dune_3 --system aarch64-darwin";
 }
