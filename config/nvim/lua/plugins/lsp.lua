c6ee0e2c8767b460992b343a4c9ffd20c1adf274
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-03-14 10:04:35 -0400

    update nvim

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,198 +1,199 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPost" },
-    cmd = { "LspInfo", "LspInstall", "LspUninstall", "Mason" },
-    dependencies = {
-      -- Plugin(s) and UI to automatically install LSPs to stdpath
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPost" },
+		cmd = { "LspInfo", "LspInstall", "LspUninstall", "Mason" },
+		dependencies = {
+			-- Plugin(s) and UI to automatically install LSPs to stdpath
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-      -- Install lsp autocompletions
-      "hrsh7th/cmp-nvim-lsp",
+			-- Install lsp autocompletions
+			"hrsh7th/cmp-nvim-lsp",
 
-      -- Progress/Status update for LSP
-      { "j-hui/fidget.nvim", opts = {} },
-    },
-    config = function()
-      local map_lsp_keybinds = require("user.keymaps").map_lsp_keybinds -- Has to load keymaps before pluginslsp
+			-- Progress/Status update for LSP
+			{ "j-hui/fidget.nvim", opts = {} },
+		},
+		config = function()
+			local map_lsp_keybinds = require("user.keymaps").map_lsp_keybinds -- Has to load keymaps before pluginslsp
 
-      -- Override tsserver diagnostics to filter out specific messages
-      local messages_to_filter = {
-        "This may be converted to an async function.",
-        "'_Assertion' is declared but never used.",
-        "'__Assertion' is declared but never used.",
-        "The signature '(data: string): string' of 'atob' is deprecated.",
-        "The signature '(data: string): string' of 'btoa' is deprecated.",
-      }
+			-- Override tsserver diagnostics to filter out specific messages
+			local messages_to_filter = {
+				"This may be converted to an async function.",
+				"'_Assertion' is declared but never used.",
+				"'__Assertion' is declared but never used.",
+				"The signature '(data: string): string' of 'atob' is deprecated.",
+				"The signature '(data: string): string' of 'btoa' is deprecated.",
+			}
 
-      local function tsserver_on_publish_diagnostics_override(_, result, ctx, config)
-        local filtered_diagnostics = {}
+			local function tsserver_on_publish_diagnostics_override(_, result, ctx, config)
+				local filtered_diagnostics = {}
 
-        for _, diagnostic in ipairs(result.diagnostics) do
-          local found = false
-          for _, message in ipairs(messages_to_filter) do
-            if diagnostic.message == message then
-              found = true
-              break
-            end
-          end
-          if not found then
-            table.insert(filtered_diagnostics, diagnostic)
-          end
-        end
+				for _, diagnostic in ipairs(result.diagnostics) do
+					local found = false
+					for _, message in ipairs(messages_to_filter) do
+						if diagnostic.message == message then
+							found = true
+							break
+						end
+					end
+					if not found then
+						table.insert(filtered_diagnostics, diagnostic)
+					end
+				end
 
-        result.diagnostics = filtered_diagnostics
+				result.diagnostics = filtered_diagnostics
 
-        vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
-      end
+				vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+			end
 
-      -- Default handlers for LSP
-      local default_handlers = {
-        ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-        ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-      }
+			-- Default handlers for LSP
+			local default_handlers = {
+				["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+				["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+			}
 
-      -- Function to run when neovim connects to a Lsp client
-      ---@diagnostic disable-next-line: unused-local
-      local on_attach = function(_client, buffer_number)
-        -- Pass the current buffer to map lsp keybinds
-        map_lsp_keybinds(buffer_number)
+			-- Function to run when neovim connects to a Lsp client
+			---@diagnostic disable-next-line: unused-local
+			local on_attach = function(_client, buffer_number)
+				-- Pass the current buffer to map lsp keybinds
+				map_lsp_keybinds(buffer_number)
 
-        -- Create a command `:Format` local to the LSP buffer
-        --[[ vim.api.nvim_buf_create_user_command(buffer_number, "Format", function(_)
+				-- Create a command `:Format` local to the LSP buffer
+				--[[ vim.api.nvim_buf_create_user_command(buffer_number, "Format", function(_)
           vim.lsp.buf.format()
         end, { desc = "LSP: Format current buffer with LSP" }) ]]
-      end
+			end
 
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP Specification.
-      --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+			-- LSP servers and clients are able to communicate to each other what features they support.
+			--  By default, Neovim doesn't support everything that is in the LSP Specification.
+			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-      -- LSP servers to install (see list here: https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers )
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- LSP Servers
-        bashls = {},
-        cssls = {},
-        html = {},
-        jsonls = {},
-        lua_ls = {
-          settings = {
-            Lua = {
-              runtime = { version = "LuaJIT" },
-              workspace = {
-                checkThirdParty = false,
-                -- Tells lua_ls where to find all the Lua files that you have loaded
-                -- for your neovim configuration.
-                library = {
-                  "${3rd}/luv/library",
-                  unpack(vim.api.nvim_get_runtime_file("", true)),
-                },
-              },
-              telemetry = { enabled = false },
-            },
-          },
-        },
-        marksman = {},
-        ocamllsp = {},
-        nil_ls = {},
-        pyright = {},
-        sqlls = {},
-        tailwindcss = {},
-        tsserver = {
-          settings = {
-            maxTsServerMemory = 12000,
-          },
-          handlers = {
-            ["textDocument/publishDiagnostics"] = vim.lsp.with(
-              tsserver_on_publish_diagnostics_override,
-              {}
-            ),
-          },
-        },
-        yamlls = {},
-      }
+			-- LSP servers to install (see list here: https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers )
+			--  Add any additional override configuration in the following tables. Available keys are:
+			--  - cmd (table): Override the default command used to start the server
+			--  - filetypes (table): Override the default list of associated filetypes for the server
+			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+			--  - settings (table): Override the default settings passed when initializing the server.
+			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+			local servers = {
+				-- LSP Servers
+				bashls = {},
+				cssls = {},
+				gleam = {},
+				html = {},
+				jsonls = {},
+				lua_ls = {
+					settings = {
+						Lua = {
+							runtime = { version = "LuaJIT" },
+							workspace = {
+								checkThirdParty = false,
+								-- Tells lua_ls where to find all the Lua files that you have loaded
+								-- for your neovim configuration.
+								library = {
+									"${3rd}/luv/library",
+									unpack(vim.api.nvim_get_runtime_file("", true)),
+								},
+							},
+							telemetry = { enabled = false },
+						},
+					},
+				},
+				marksman = {},
+				ocamllsp = {},
+				nil_ls = {},
+				pyright = {},
+				sqlls = {},
+				tailwindcss = {},
+				tsserver = {
+					settings = {
+						maxTsServerMemory = 12000,
+					},
+					handlers = {
+						["textDocument/publishDiagnostics"] = vim.lsp.with(
+							tsserver_on_publish_diagnostics_override,
+							{}
+						),
+					},
+				},
+				yamlls = {},
+			}
 
-      local formatters = {
-        prettierd = {},
-        stylua = {},
-      }
+			local formatters = {
+				prettierd = {},
+				stylua = {},
+			}
 
-      local linters = {
-        eslint_d = {},
-      }
+			local linters = {
+				eslint_d = {},
+			}
 
-      local manually_installed_servers = { "ocamllsp" }
+			local manually_installed_servers = { "ocamllsp", "gleam" }
 
-      local mason_tools_to_install = vim.tbl_keys(vim.tbl_deep_extend("force", {}, servers, formatters, linters))
+			local mason_tools_to_install = vim.tbl_keys(vim.tbl_deep_extend("force", {}, servers, formatters, linters))
 
-      local ensure_installed = vim.tbl_filter(function(name)
-        return not vim.tbl_contains(manually_installed_servers, name)
-      end, mason_tools_to_install)
+			local ensure_installed = vim.tbl_filter(function(name)
+				return not vim.tbl_contains(manually_installed_servers, name)
+			end, mason_tools_to_install)
 
-      require("mason-tool-installer").setup({
-        auto_update = true,
-        run_on_start = true,
-        start_delay = 3000,
-        debounce_hours = 12,
-        ensure_installed = ensure_installed,
-      })
+			require("mason-tool-installer").setup({
+				auto_update = true,
+				run_on_start = true,
+				start_delay = 3000,
+				debounce_hours = 12,
+				ensure_installed = ensure_installed,
+			})
 
-      -- Iterate over our servers and set them up
-      for name, config in pairs(servers) do
-        require("lspconfig")[name].setup({
-          capabilities = capabilities,
-          filetypes = config.filetypes,
-          handlers = vim.tbl_deep_extend("force", {}, default_handlers, config.handlers or {}),
-          on_attach = on_attach,
-          settings = config.settings,
-        })
-      end
+			-- Iterate over our servers and set them up
+			for name, config in pairs(servers) do
+				require("lspconfig")[name].setup({
+					capabilities = capabilities,
+					filetypes = config.filetypes,
+					handlers = vim.tbl_deep_extend("force", {}, default_handlers, config.handlers or {}),
+					on_attach = on_attach,
+					settings = config.settings,
+				})
+			end
 
-      -- Setup mason so it can manage 3rd party LSP servers
-      require("mason").setup({
-        ui = {
-          border = "rounded",
-        },
-      })
+			-- Setup mason so it can manage 3rd party LSP servers
+			require("mason").setup({
+				ui = {
+					border = "rounded",
+				},
+			})
 
-      require("mason-lspconfig").setup()
+			require("mason-lspconfig").setup()
 
-      -- Configure borderd for LspInfo ui
-      require("lspconfig.ui.windows").default_options.border = "rounded"
+			-- Configure borderd for LspInfo ui
+			require("lspconfig.ui.windows").default_options.border = "rounded"
 
-      -- Configure diagnostics border
-      vim.diagnostic.config({
-        float = {
-          border = "rounded",
-        },
-      })
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      notify_on_error = true,
-      format_after_save = {
-        async = true,
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        javascript = { { "eslint_d", "eslint" }, { "prettierd", "prettier" } },
-        typescript = { { "eslint_d", "eslint" }, { "prettierd", "prettier" } },
-        lua = { "stylua" },
-      },
-    },
-  },
+			-- Configure diagnostics border
+			vim.diagnostic.config({
+				float = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			notify_on_error = true,
+			format_after_save = {
+				async = true,
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+			formatters_by_ft = {
+				javascript = { { "eslint_d", "eslint" }, { "prettierd", "prettier" } },
+				typescript = { { "eslint_d", "eslint" }, { "prettierd", "prettier" } },
+				lua = { "stylua" },
+			},
+		},
+	},
 }
--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -17,43 +17,43 @@ nnoremap("<space>", "<nop>")
 
 -- Window +  better kitty navigation
 nnoremap("<C-j>", function()
-  if vim.fn.exists(":KittyNavigateDown") ~= 0 and TERM == "xterm-kitty" then
-    vim.cmd.KittyNavigateDown()
-  elseif vim.fn.exists(":NvimTmuxNavigateDown") ~= 0 then
-    vim.cmd.NvimTmuxNavigateDown()
-  else
-    vim.cmd.wincmd("j")
-  end
+	if vim.fn.exists(":KittyNavigateDown") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateDown()
+	elseif vim.fn.exists(":NvimTmuxNavigateDown") ~= 0 then
+		vim.cmd.NvimTmuxNavigateDown()
+	else
+		vim.cmd.wincmd("j")
+	end
 end)
 
 nnoremap("<C-k>", function()
-  if vim.fn.exists(":KittyNavigateUp") ~= 0 and TERM == "xterm-kitty" then
-    vim.cmd.KittyNavigateUp()
-  elseif vim.fn.exists(":NvimTmuxNavigateUp") ~= 0 then
-    vim.cmd.NvimTmuxNavigateUp()
-  else
-    vim.cmd.wincmd("k")
-  end
+	if vim.fn.exists(":KittyNavigateUp") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateUp()
+	elseif vim.fn.exists(":NvimTmuxNavigateUp") ~= 0 then
+		vim.cmd.NvimTmuxNavigateUp()
+	else
+		vim.cmd.wincmd("k")
+	end
 end)
 
 nnoremap("<C-l>", function()
-  if vim.fn.exists(":KittyNavigateRight") ~= 0 and TERM == "xterm-kitty" then
-    vim.cmd.KittyNavigateRight()
-  elseif vim.fn.exists(":NvimTmuxNavigateRight") ~= 0 then
-    vim.cmd.NvimTmuxNavigateRight()
-  else
-    vim.cmd.wincmd("l")
-  end
+	if vim.fn.exists(":KittyNavigateRight") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateRight()
+	elseif vim.fn.exists(":NvimTmuxNavigateRight") ~= 0 then
+		vim.cmd.NvimTmuxNavigateRight()
+	else
+		vim.cmd.wincmd("l")
+	end
 end)
 
 nnoremap("<C-h>", function()
-  if vim.fn.exists(":KittyNavigateLeft") ~= 0 and TERM == "xterm-kitty" then
-    vim.cmd.KittyNavigateLeft()
-  elseif vim.fn.exists(":NvimTmuxNavigateLeft") ~= 0 then
-    vim.cmd.NvimTmuxNavigateLeft()
-  else
-    vim.cmd.wincmd("h")
-  end
+	if vim.fn.exists(":KittyNavigateLeft") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateLeft()
+	elseif vim.fn.exists(":NvimTmuxNavigateLeft") ~= 0 then
+		vim.cmd.NvimTmuxNavigateLeft()
+	else
+		vim.cmd.wincmd("h")
+	end
 end)
 
 -- Swap between last two buffers
@@ -70,7 +70,7 @@ nnoremap("<leader>z", "<cmd>wq<cr>", { silent = false })
 
 -- Map Oil to <leader>e
 nnoremap("<leader>e", function()
-  require("oil").toggle_float()
+	require("oil").toggle_float()
 end)
 
 -- Center buffer while navigating
@@ -90,20 +90,20 @@ nnoremap("#", "#zz")
 
 -- Press 'S' for quick find/replace for the word under the cursor
 nnoremap("S", function()
-  local cmd = ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>"
-  local keys = vim.api.nvim_replace_termcodes(cmd, true, false, true)
-  vim.api.nvim_feedkeys(keys, "n", false)
+	local cmd = ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>"
+	local keys = vim.api.nvim_replace_termcodes(cmd, true, false, true)
+	vim.api.nvim_feedkeys(keys, "n", false)
 end)
 
 -- Open Spectre for global find/replace
 nnoremap("<leader>S", function()
-  require("spectre").toggle()
+	require("spectre").toggle()
 end)
 
 -- Open Spectre for global find/replace for the word under the cursor in normal mode
 -- TODO Fix, currently being overriden by Telescope
 nnoremap("<leader>sw", function()
-  require("spectre").open_visual({ select_word = true })
+	require("spectre").open_visual({ select_word = true })
 end, { desc = "Search current word" })
 
 -- Press 'H', 'L' to jump to start/end of a line (first/last char)
@@ -120,45 +120,45 @@ nnoremap("<leader>no", "<cmd>noh<cr>")
 
 -- Goto next diagnostic of any severity
 nnoremap("]d", function()
-  vim.diagnostic.goto_next({})
-  vim.api.nvim_feedkeys("zz", "n", false)
+	vim.diagnostic.goto_next({})
+	vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Goto previous diagnostic of any severity
 nnoremap("[d", function()
-  vim.diagnostic.goto_prev({})
-  vim.api.nvim_feedkeys("zz", "n", false)
+	vim.diagnostic.goto_prev({})
+	vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Goto next error diagnostic
 nnoremap("]e", function()
-  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
-  vim.api.nvim_feedkeys("zz", "n", false)
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+	vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Goto previous error diagnostic
 nnoremap("[e", function()
-  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
-  vim.api.nvim_feedkeys("zz", "n", false)
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+	vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Goto next warning diagnostic
 nnoremap("]w", function()
-  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
-  vim.api.nvim_feedkeys("zz", "n", false)
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
+	vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Goto previous warning diagnostic
 nnoremap("[w", function()
-  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
-  vim.api.nvim_feedkeys("zz", "n", false)
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+	vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Open the diagnostic under the cursor in a float window
 nnoremap("<leader>d", function()
-  vim.diagnostic.open_float({
-    border = "rounded",
-  })
+	vim.diagnostic.open_float({
+		border = "rounded",
+	})
 end)
 
 -- Place all dignostics into a qflist
@@ -184,7 +184,7 @@ nnoremap("<leader>=", "<C-w>=")
 
 -- Press leader f to format
 nnoremap("<leader>f", function()
-  vim.lsp.buf.format()
+	vim.lsp.buf.format()
 end, { desc = "Format the current buffer" })
 
 -- Press leader rw to rotate open windows
@@ -199,134 +199,134 @@ nnoremap("<leader>tc", ":TSC<cr>", { desc = "[T]ypeScript [C]ompile" })
 -- Harpoon keybinds --
 -- Open harpoon ui
 nnoremap("<leader>ho", function()
-  harpoon_ui.toggle_quick_menu()
+	harpoon_ui.toggle_quick_menu()
 end)
 
 -- Add current file to harpoon
 nnoremap("<leader>ha", function()
-  harpoon_mark.add_file()
+	harpoon_mark.add_file()
 end)
 
 -- Remove current file from harpoon
 nnoremap("<leader>hr", function()
-  harpoon_mark.rm_file()
+	harpoon_mark.rm_file()
 end)
 
 -- Remove all files from harpoon
 nnoremap("<leader>hc", function()
-  harpoon_mark.clear_all()
+	harpoon_mark.clear_all()
 end)
 
 -- Quickly jump to harpooned files
 nnoremap("<leader>1", function()
-  harpoon_ui.nav_file(1)
+	harpoon_ui.nav_file(1)
 end)
 
 nnoremap("<leader>2", function()
-  harpoon_ui.nav_file(2)
+	harpoon_ui.nav_file(2)
 end)
 
 nnoremap("<leader>3", function()
-  harpoon_ui.nav_file(3)
+	harpoon_ui.nav_file(3)
 end)
 
 nnoremap("<leader>4", function()
-  harpoon_ui.nav_file(4)
+	harpoon_ui.nav_file(4)
 end)
 
 nnoremap("<leader>5", function()
-  harpoon_ui.nav_file(5)
+	harpoon_ui.nav_file(5)
 end)
 
 -- Git keymaps --
 nnoremap("<leader>gb", ":Gitsigns toggle_current_line_blame<cr>")
 nnoremap("<leader>gf", function()
-  local cmd = {
-    "sort",
-    "-u",
-    "<(git diff --name-only --cached)",
-    "<(git diff --name-only)",
-    "<(git diff --name-only --diff-filter=U)",
-  }
+	local cmd = {
+		"sort",
+		"-u",
+		"<(git diff --name-only --cached)",
+		"<(git diff --name-only)",
+		"<(git diff --name-only --diff-filter=U)",
+	}
 
-  if not utils.is_git_directory() then
-    vim.notify(
-      "Current project is not a git directory",
-      vim.log.levels.WARN,
-      { title = "Telescope Git Files", git_command = cmd }
-    )
-  else
-    require("telescope.builtin").git_files()
-  end
+	if not utils.is_git_directory() then
+		vim.notify(
+			"Current project is not a git directory",
+			vim.log.levels.WARN,
+			{ title = "Telescope Git Files", git_command = cmd }
+		)
+	else
+		require("telescope.builtin").git_files()
+	end
 end, { desc = "Search [G]it [F]iles" })
 
 -- Telescope keybinds --
 nnoremap("<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
 nnoremap("<leader>sb", require("telescope.builtin").buffers, { desc = "[S]earch Open [B]uffers" })
 nnoremap("<leader>sf", function()
-  require("telescope.builtin").find_files({ hidden = true })
+	require("telescope.builtin").find_files({ hidden = true })
 end, { desc = "[S]earch [F]iles" })
 nnoremap("<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
 nnoremap("<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
 
 nnoremap("<leader>sc", function()
-  require("telescope.builtin").commands(require("telescope.themes").get_dropdown({
-    previewer = false,
-  }))
+	require("telescope.builtin").commands(require("telescope.themes").get_dropdown({
+		previewer = false,
+	}))
 end, { desc = "[S]earch [C]ommands" })
 
 nnoremap("<leader>/", function()
-  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-    previewer = false,
-  }))
+	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		previewer = false,
+	}))
 end, { desc = "[/] Fuzzily search in current buffer]" })
 
 nnoremap("<leader>ss", function()
-  require("telescope.builtin").spell_suggest(require("telescope.themes").get_dropdown({
-    previewer = false,
-  }))
+	require("telescope.builtin").spell_suggest(require("telescope.themes").get_dropdown({
+		previewer = false,
+	}))
 end, { desc = "[S]earch [S]pelling suggestions" })
 
 -- LSP Keybinds (exports a function to be used in ../../after/plugin/lsp.lua b/c we need a reference to the current buffer) --
 M.map_lsp_keybinds = function(buffer_number)
-  nnoremap("<leader>rn", vim.lsp.buf.rename, { desc = "LSP: [R]e[n]ame", buffer = buffer_number })
-  nnoremap("<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: [C]ode [A]ction", buffer = buffer_number })
+	nnoremap("<leader>rn", vim.lsp.buf.rename, { desc = "LSP: [R]e[n]ame", buffer = buffer_number })
+	nnoremap("<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: [C]ode [A]ction", buffer = buffer_number })
 
-  nnoremap("gd", vim.lsp.buf.definition, { desc = "LSP: [G]oto [D]efinition", buffer = buffer_number })
+	nnoremap("gd", vim.lsp.buf.definition, { desc = "LSP: [G]oto [D]efinition", buffer = buffer_number })
 
-  -- Telescope LSP keybinds --
-  nnoremap(
-    "gr",
-    require("telescope.builtin").lsp_references,
-    { desc = "LSP: [G]oto [R]eferences", buffer = buffer_number }
-  )
+	-- Telescope LSP keybinds --
+	nnoremap(
+		"gr",
+		require("telescope.builtin").lsp_references,
+		{ desc = "LSP: [G]oto [R]eferences", buffer = buffer_number }
+	)
 
-  nnoremap(
-    "gi",
-    require("telescope.builtin").lsp_implementations,
-    { desc = "LSP: [G]oto [I]mplementation", buffer = buffer_number }
-  )
+	nnoremap(
+		"gi",
+		require("telescope.builtin").lsp_implementations,
+		{ desc = "LSP: [G]oto [I]mplementation", buffer = buffer_number }
+	)
 
-  nnoremap(
-    "<leader>bs",
-    require("telescope.builtin").lsp_document_symbols,
-    { desc = "LSP: [B]uffer [S]ymbols", buffer = buffer_number }
-  )
+	nnoremap(
+		"<leader>bs",
+		require("telescope.builtin").lsp_document_symbols,
+		{ desc = "LSP: [B]uffer [S]ymbols", buffer = buffer_number }
+	)
 
-  nnoremap(
-    "<leader>ps",
-    require("telescope.builtin").lsp_workspace_symbols,
-    { desc = "LSP: [P]roject [S]ymbols", buffer = buffer_number }
-  )
+	nnoremap(
+		"<leader>ps",
+		require("telescope.builtin").lsp_workspace_symbols,
+		{ desc = "LSP: [P]roject [S]ymbols", buffer = buffer_number }
+	)
 
-  -- See `:help K` for why this keymap
-  nnoremap("K", vim.lsp.buf.hover, { desc = "LSP: Hover Documentation", buffer = buffer_number })
-  nnoremap("<leader>k", vim.lsp.buf.signature_help, { desc = "LSP: Signature Documentation", buffer = buffer_number })
-  inoremap("<C-k>", vim.lsp.buf.signature_help, { desc = "LSP: Signature Documentation", buffer = buffer_number })
+	-- See `:help K` for why this keymap
+	nnoremap("K", vim.lsp.buf.hover, { desc = "LSP: Hover Documentation", buffer = buffer_number })
+	nnoremap("<leader>k", vim.lsp.buf.signature_help, { desc = "LSP: Signature Documentation", buffer = buffer_number })
+	inoremap("<C-k>", vim.lsp.buf.signature_help, { desc = "LSP: Signature Documentation", buffer = buffer_number })
 
-  -- Lesser used LSP functionality
-  nnoremap("gD", vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration", buffer = buffer_number })
-  nnoremap("td", vim.lsp.buf.type_definition, { desc = "LSP: [T]ype [D]efinition", buffer = buffer_number })
+	-- Lesser used LSP functionality
+	nnoremap("gD", vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration", buffer = buffer_number })
+	nnoremap("td", vim.lsp.buf.type_definition, { desc = "LSP: [T]ype [D]efinition", buffer = buffer_number })
 end
 
 -- Symbol Outline keybind
@@ -334,7 +334,7 @@ nnoremap("<leader>so", ":SymbolsOutline<cr>")
 
 -- Open Copilot panel
 nnoremap("<leader>oc", function()
-  require("copilot.panel").open({})
+	require("copilot.panel").open({})
 end, { desc = "[O]pen [C]opilot panel" })
 
 -- nvim-ufo keybinds
@@ -357,18 +357,18 @@ vnoremap("H", "^")
 vnoremap("<A-j>", ":m '>+1<CR>gv=gv")
 vnoremap("<A-k>", ":m '<-2<CR>gv=gv")
 
+xnoremap("<leader>p", '"_dP')
+
 -- Reselect the last visual selection
 xnoremap("<<", function()
-  xnoremap("<leader>p", '"_dP')
-
-  -- Move selected text up/down in visual mode
-  vim.cmd("normal! <<")
-  vim.cmd("normal! gv")
+	-- Move selected text up/down in visual mode
+	vim.cmd("normal! <<")
+	vim.cmd("normal! gv")
 end)
 
 xnoremap(">>", function()
-  vim.cmd("normal! >>")
-  vim.cmd("normal! gv")
+	vim.cmd("normal! >>")
+	vim.cmd("normal! gv")
 end)
 
 -- Terminal --
