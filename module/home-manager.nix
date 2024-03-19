4c21d10217b3db7594ba9d2a6005987065e5e24b
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-03-19 14:38:46 -0400

    update git

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -14,6 +14,7 @@ in {
 
   home.packages = with pkgs; [
     fd
+    gnused
     mono-lisa-font
     ripgrep
     tree
@@ -67,14 +68,16 @@ in {
     ];
     aliases = {
       staash = "stash --all";
-      lg = "lg1";
-      lg1 = "lg1-specific --all";
-      lg2 = "lg2-specific --all";
-      lg3 = "lg3-specific --all";
-
-      lg1-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
-      lg2-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
-      lg3-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'";
+      lg = ''
+        git log \
+        --color=always  \
+        --graph  \
+        --decorate \
+        --abbrev-commit \
+        --date=relative \
+        --format=format:'  %C(auto)%h %C(bold blue)%an %C(bold green)%ad %C(auto)%d%n  │  %C(white)%s%C(reset)' --all -n 20 | \
+        sed 's/\(\(origin\/\)\?[a-zA-Z0-9-]*\/[a-zA-Z]*-[0-9]*\)-[a-zA-Z0-9-]*/\1/g'
+      '';
     };
     lfs = {
       enable = true;
