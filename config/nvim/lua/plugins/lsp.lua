8844dd1469e95fc947b66cd36d8772899492bfe4
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-05-31 09:48:45 -0400

    wip

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -208,23 +208,23 @@ return {
 			})
 		end,
 	},
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		opts = {
-			notify_on_error = false,
-			format_after_save = {
-				async = true,
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-			formatters_by_ft = {
-				javascript = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
-				lua = { "stylua" },
-			},
-		},
-	},
+	-- {
+	-- 	"stevearc/conform.nvim",
+	-- 	event = { "BufWritePre" },
+	-- 	cmd = { "ConformInfo" },
+	-- 	opts = {
+	-- 		notify_on_error = false,
+	-- 		format_after_save = {
+	-- 			async = true,
+	-- 			timeout_ms = 500,
+	-- 			lsp_fallback = true,
+	-- 		},
+	-- 		formatters_by_ft = {
+	-- 			javascript = { { "prettierd", "prettier" } },
+	-- 			typescript = { { "prettierd", "prettier" } },
+	-- 			typescriptreact = { { "prettierd", "prettier" } },
+	-- 			lua = { "stylua" },
+	-- 		},
+	-- 	},
+	-- },
 }
