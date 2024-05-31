f98d63fdb7c49f348338146673e921d3d81c3b23
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-05-31 09:51:55 -0400

    wip

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -5,7 +5,7 @@ local tnoremap = require("user.keymap_utils").tnoremap
 local xnoremap = require("user.keymap_utils").xnoremap
 local harpoon_ui = require("harpoon.ui")
 local harpoon_mark = require("harpoon.mark")
-local conform = require("conform")
+-- local conform = require("conform")
 local utils = require("user.utils")
 
 local M = {}
