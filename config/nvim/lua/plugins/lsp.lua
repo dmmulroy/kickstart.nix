4d10d119ac710aac11262b0280ca6809362d04f7
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-10-11 12:28:01 -0400

    nix flake update

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -108,7 +108,7 @@ return {
 				tailwindcss = {},
 				ts_ls = {
 					settings = {
-						maxts_lsMemory = 12288,
+						maxTsServerMemory = 12288,
 						typescript = {
 							inlayHints = ts_ls_inlay_hints,
 						},
